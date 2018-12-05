03b01b25b5ede49696eab70b6271c1d6d33df7db
Jaiden Mispy <jaiden@mispy.me>
2018-12-05 13:52:43 +0800

    Add day 5 solution

--- template.js
+++ template.js
@@ -2,8 +2,25 @@ const { _, log, runSamples, ...aoc } = require('./dist/util')
 
 /** @param {string[]} lines */
 function main(lines) {
-    log(aoc.ints("#72 @ 930,561: 11x11"))
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 runSamples(main, [
 `
